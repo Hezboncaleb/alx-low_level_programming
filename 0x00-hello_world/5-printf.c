8bc42dc93d996403a6482456c407be1ab56f73f4
Hezboncaleb <hezboncaleb@gmail.com>
2023-06-15 16:42:22 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints a newline uisng the printf function
+ *
+ * Return: O on Suceess, non-zero otherwise
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
