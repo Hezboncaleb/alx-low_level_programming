ed4fb96a403dd3f3dd56ba990b212fa99a2c6f5c
Hezboncaleb <hezboncaleb@gmail.com>
2023-06-15 15:57:53 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a newline using the puts function
+ *
+ * Return : 0 on Success
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
