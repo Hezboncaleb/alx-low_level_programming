0bbc822073f0af0acbc3c460177aab36e7c2fb8d
Hezboncaleb <hezboncaleb@gmail.com>
2023-06-26 14:59:49 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - writes a function that points to an int and updates
+ *               the value of the address to 98.
+ * @n: int declared varialble name
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+}
